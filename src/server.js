b014c778f4f08f6530775926c6d62480d95f9f4b
Jonathon Plumb <jon.plumb89@outlook.com>
2024-11-09 20:29:11 -0500

    update5

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const adminRoutes = require('./routes/adminRoutes');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // MongoDB connection URI
-const uri = "mongodb+srv://jonplumb89:KEXeUDnC9Lo5kxLG@the-sandtrap.rzuyf.mongodb.net/the-sandtrap?retryWrites=true&w=majority";
+const uri = "mongodb+srv://jonplumb89:KEXeUDnC9Lo5kxLG@the-sandtrap.rzuyf.mongodb.net/?retryWrites=true&w=majority&appName=the-sandtrap";
+
 
 // Set up Express app
 const app = express();
